86a93c5c53e5857c315b22897f11e9a3194912b9
Michael Perez <michaelp193@gmail.com>
2014-05-24 06:02:18 -0400

    jslint

--- js/main.js
+++ js/main.js
@@ -1,40 +1,42 @@
-(function() {
-  var is_touch_device = 'ontouchstart' in document.documentElement;
-  function ajaxIn(page) {
-    $.ajax({
-      url: "html/" + page,
-      type: "GET",
-      dataType: "html",
-      cache: true
-    })
-    .fail(function() {
-      alert("Sorry page couldn't load");
-    })
-    .done(function(html) {
-      history.pushState(null, null, page);
-      $container = $(".container");
-      $container.html(html);
-      $('html, body').animate({scrollTop: $container.offset().top}, 750);
-      $container.fadeIn("slow");
-      $("a[href='index.html']").on("click",function(event) {
-        event.preventDefault();
-        $('html, body').animate({scrollTop: 0}, 750);
-        history.pushState(null, null, "index.html");
-    });
-      if (is_touch_device) {
-        $(".secondNav li").on("click", function() {
-          $("nav li ul").toggleClass("expose");
+"use strict";
+(function () {
+    var is_touch_device = document.documentElement.hasOwnProperty('ontouchstart');
+    function ajaxIn(page) {
+        $.ajax({
+            url: "html/" + page,
+            type: "GET",
+            dataType: "html",
+            cache: true
+        })
+            .fail(function () {
+                alert("Sorry page couldn't load");
+            })
+            .done(function (html) {
+                history.pushState(null, null, page);
+                var $container = $(".container");
+                $container.html(html);
+                $('html, body').animate({scrollTop: $container.offset().top}, 750);
+                $container.fadeIn("slow");
+                $("a[href='index.html']").on("click", function (event) {
+                    event.preventDefault();
+                    $('html, body').animate({scrollTop: 0}, 750);
+                    history.pushState(null, null, "index.html");
+                });
+                if (is_touch_device) {
+                    $(".secondNav li").on("touchstart", function (event) {
+                        event.stropPropagation();
+                        event.preventDefault();
+                        $("nav li ul").toggleClass("expose");
+                    });
+                }
+            });
+        $(document).ready(function () {
+            var $navItems = $(".mainNav a");
+            $navItems.on("click", function (event) {
+                event.preventDefault();
+                var requestPage = $(event.target).attr("href");
+                console.log(requestPage);
+                ajaxIn(requestPage);
+            });
         });
-      }
-    });
-  }
-  $(document).ready(function() {
-    var $navItems = $(".mainNav a");
-    $navItems.on("click", function(event) {
-      event.preventDefault();
-      var requestPage = $(event.target).attr("href")
-      console.log(requestPage);
-      ajaxIn(requestPage);
-    });
-});
-})(jQuery);
+    }(jQuery));
